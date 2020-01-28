0ff13e08a829ce068b91ab36fa1ece7a1e6578bd
darkarth80 <26891629+darkarth80@users.noreply.github.com>
2020-01-28 16:41:41 -0600

    Se agrega soporte para wars.

--- src/main/java/com/softtek/consumewebservice/ConsumeWebServiceApplication.java
+++ src/main/java/com/softtek/consumewebservice/ConsumeWebServiceApplication.java
@@ -2,12 +2,18 @@ package com.softtek.consumewebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConsumeWebServiceApplication {
+public class ConsumeWebServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumeWebServiceApplication.class, args);
+    }
+
+    public SpringApplicationBuilder cofigure(SpringApplicationBuilder builder) {
+        return builder.sources(ConsumeWebServiceApplication.class);
     }
 
 }
