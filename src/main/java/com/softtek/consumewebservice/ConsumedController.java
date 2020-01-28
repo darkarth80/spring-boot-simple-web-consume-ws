352691f04dc1219ec60e30e68cd976a9be3ad8a6
darkarth80 <26891629+darkarth80@users.noreply.github.com>
2020-01-28 07:08:12 -0600

    Se agregan archivos y modificaciones

--- src/main/java/com/softtek/consumewebservice/ConsumedController.java
+++ src/main/java/com/softtek/consumewebservice/ConsumedController.java
@@ -1,17 +1,21 @@
 package com.softtek.consumewebservice;
 
+import com.softtek.consumewebservice.dto.Walmart;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class ConsumedController {
 
-	@GetMapping("/")
-	public String greeting(@RequestParam(name="texto", required=false, defaultValue="Hola") String texto, Model model) {
-		model.addAttribute("texto", texto);
-		return "greeting";
+	@GetMapping("/getResponse")
+	public String greeting(@RequestParam(name="obj", required=false) String obj, Model model) {
+		Walmart wal = new RestTemplate().getForObject("http://localhost:8080/", Walmart.class);
+		model.addAttribute("obj", wal);
+		return "";
 	}
 
 }
